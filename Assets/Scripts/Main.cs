31589611bb099deda1fe5884456ea3130bf199d4
Ho <hxptow@163.com>
2020-09-09 21:07:03 +0800

    缩放逻辑使用PointEventData

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    private int n = 100;
+    private bool ensureExit = false;
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if(ensureExit)
+            {
+                Application.Quit();
+            }
+            else
+            {
+                ensureExit = true;
+                n = 100;
+            }
+        }
+
+        if(ensureExit)
+        {
+            n--;
+            if(n <= 0)
+            {
+                ensureExit = false;
+            }
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (ensureExit)
+        {
+            GUILayout.Space(100);
+            GUILayout.Label("再次按Esc退出");
+        }
+    }
+}
